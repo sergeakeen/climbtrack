90b3138ca8072107ec8cd667a07ad104747e9c6c
Serge Akeen <akiniazov@gmail.com>
2024-10-15 01:36:44 +0200

    adjusted grades

--- grades.js
+++ grades.js
@@ -123,83 +123,82 @@ export const grades = [
 { id: 115, original: 'french', type: 'Sport Climbing', french: '6b+', american: '5.10d', gradeValue: 8, color: '#51b6f1' },
 
 // GradeValue 9
-{ id: 116, original: 'american', type: 'Sport Climbing', american: '5.11a', french: '6c', gradeValue: 9, color: '#60a5fa' },
+{ id: 116, original: 'american', type: 'Sport Climbing', american: '5.11a', french: '6b+/6c', gradeValue: 9, color: '#60a5fa' },
 { id: 117, original: 'french', type: 'Sport Climbing', french: '6c', american: '5.11a', gradeValue: 9, color: '#60a5fa' },
 
 // GradeValue 10
-{ id: 118, original: 'american', type: 'Sport Climbing', american: '5.11b', french: '6c+', gradeValue: 10, color: '#f1a3a3' },
+{ id: 118, original: 'american', type: 'Sport Climbing', american: '5.11b', french: '6c/6c+', gradeValue: 10, color: '#f1a3a3' },
 { id: 119, original: 'french', type: 'Sport Climbing', french: '6c+', american: '5.11b', gradeValue: 10, color: '#f1a3a3' },
 
 // GradeValue 11
-{ id: 120, original: 'american', type: 'Sport Climbing', american: '5.11c', french: '7a', gradeValue: 11, color: '#ec8a8a' },
-{ id: 121, original: 'french', type: 'Sport Climbing', french: '7a', american: '5.11c', gradeValue: 11, color: '#ec8a8a' },
+{ id: 120, original: 'american', type: 'Sport Climbing', american: '5.11c', french: '6c+/7a', gradeValue: 11, color: '#ec8a8a' },
 
 // GradeValue 12
-{ id: 122, original: 'american', type: 'Sport Climbing', american: '5.11d', french: '7a+', gradeValue: 12, color: '#e87171' },
-{ id: 123, original: 'french', type: 'Sport Climbing', french: '7a+', american: '5.11d', gradeValue: 12, color: '#e87171' },
+{ id: 122, original: 'american', type: 'Sport Climbing', american: '5.11d', french: '7a', gradeValue: 12, color: '#e87171' },
+{ id: 123, original: 'french', type: 'Sport Climbing', french: '7a', american: '5.11d', gradeValue: 12, color: '#e87171' },
 
 // GradeValue 13
-{ id: 124, original: 'american', type: 'Sport Climbing', american: '5.12a', french: '7b', gradeValue: 13, color: '#808080' },
-{ id: 125, original: 'french', type: 'Sport Climbing', french: '7b', american: '5.12a', gradeValue: 13, color: '#808080' },
+{ id: 124, original: 'american', type: 'Sport Climbing', american: '5.12a', french: '7a+', gradeValue: 13, color: '#808080' },
+{ id: 125, original: 'french', type: 'Sport Climbing', french: '7a+', american: '5.12a', gradeValue: 13, color: '#808080' },
 
 // GradeValue 14
-{ id: 126, original: 'american', type: 'Sport Climbing', american: '5.12b', french: '7b+', gradeValue: 14, color: '#404040' },
-{ id: 127, original: 'french', type: 'Sport Climbing', french: '7b+', american: '5.12b', gradeValue: 14, color: '#404040' },
+{ id: 126, original: 'american', type: 'Sport Climbing', american: '5.12b', french: '7b', gradeValue: 14, color: '#404040' },
+{ id: 127, original: 'french', type: 'Sport Climbing', french: '7b', american: '5.12b', gradeValue: 14, color: '#404040' },
 
 // GradeValue 15
-{ id: 128, original: 'american', type: 'Sport Climbing', american: '5.12c', french: '7c', gradeValue: 15, color: '#000000' },
-{ id: 129, original: 'french', type: 'Sport Climbing', french: '7c', american: '5.12c', gradeValue: 15, color: '#000000' },
+{ id: 128, original: 'american', type: 'Sport Climbing', american: '5.12c', french: '7b+', gradeValue: 15, color: '#000000' },
+{ id: 129, original: 'french', type: 'Sport Climbing', french: '7b+', american: '5.12c', gradeValue: 15, color: '#000000' },
 
 // GradeValue 16
-{ id: 130, original: 'american', type: 'Sport Climbing', american: '5.12d', french: '7c+', gradeValue: 16, color: '#fdb78b' },
-{ id: 131, original: 'french', type: 'Sport Climbing', french: '7c+', american: '5.12d', gradeValue: 16, color: '#fdb78b' },
+{ id: 130, original: 'american', type: 'Sport Climbing', american: '5.12d', french: '7c', gradeValue: 16, color: '#fdb78b' },
+{ id: 131, original: 'french', type: 'Sport Climbing', french: '7c', american: '5.12d', gradeValue: 16, color: '#fdb78b' },
 
 // GradeValue 17
-{ id: 132, original: 'american', type: 'Sport Climbing', american: '5.13a', french: '8a', gradeValue: 17, color: '#fca464' },
-{ id: 133, original: 'french', type: 'Sport Climbing', french: '8a', american: '5.13a', gradeValue: 17, color: '#fca464' },
+{ id: 132, original: 'american', type: 'Sport Climbing', american: '5.13a', french: '7c+', gradeValue: 17, color: '#fca464' },
+{ id: 133, original: 'french', type: 'Sport Climbing', french: '7c+', american: '5.13a', gradeValue: 17, color: '#fca464' },
 
 // GradeValue 18
-{ id: 134, original: 'american', type: 'Sport Climbing', american: '5.13b', french: '8a+', gradeValue: 18, color: '#fb923c' },
-{ id: 135, original: 'french', type: 'Sport Climbing', french: '8a+', american: '5.13b', gradeValue: 18, color: '#fb923c' },
+{ id: 134, original: 'american', type: 'Sport Climbing', american: '5.13b', french: '8a', gradeValue: 18, color: '#fb923c' },
+{ id: 135, original: 'french', type: 'Sport Climbing', french: '8a', american: '5.13b', gradeValue: 18, color: '#fb923c' },
 
 // GradeValue 19
-{ id: 136, original: 'american', type: 'Sport Climbing', american: '5.13c', french: '8b', gradeValue: 19, color: '#d8b4fe' },
-{ id: 137, original: 'french', type: 'Sport Climbing', french: '8b', american: '5.13c', gradeValue: 19, color: '#d8b4fe' },
+{ id: 136, original: 'american', type: 'Sport Climbing', american: '5.13c', french: '8a+', gradeValue: 19, color: '#d8b4fe' },
+{ id: 137, original: 'french', type: 'Sport Climbing', french: '8a+', american: '5.13c', gradeValue: 19, color: '#d8b4fe' },
 
 // GradeValue 20
-{ id: 138, original: 'american', type: 'Sport Climbing', american: '5.13d', french: '8b+', gradeValue: 20, color: '#c084fc' },
-{ id: 139, original: 'french', type: 'Sport Climbing', french: '8b+', american: '5.13d', gradeValue: 20, color: '#c084fc' },
+{ id: 138, original: 'american', type: 'Sport Climbing', american: '5.13d', french: '8b', gradeValue: 20, color: '#c084fc' },
+{ id: 139, original: 'french', type: 'Sport Climbing', french: '8b', american: '5.13d', gradeValue: 20, color: '#c084fc' },
 
 // GradeValue 21
-{ id: 140, original: 'american', type: 'Sport Climbing', american: '5.14a', french: '8c', gradeValue: 21, color: '#a855f7' },
-{ id: 141, original: 'french', type: 'Sport Climbing', french: '8c', american: '5.14a', gradeValue: 21, color: '#a855f7' },
+{ id: 140, original: 'american', type: 'Sport Climbing', american: '5.14a', french: '8b+', gradeValue: 21, color: '#a855f7' },
+{ id: 141, original: 'french', type: 'Sport Climbing', french: '8b+', american: '5.14a', gradeValue: 21, color: '#a855f7' },
 
 // GradeValue 22
-{ id: 142, original: 'american', type: 'Sport Climbing', american: '5.14b', french: '8c+', gradeValue: 22, color: '#9333ea' },
-{ id: 143, original: 'french', type: 'Sport Climbing', french: '8c+', american: '5.14b', gradeValue: 22, color: '#9333ea' },
+{ id: 142, original: 'american', type: 'Sport Climbing', american: '5.14b', french: '8c', gradeValue: 22, color: '#9333ea' },
+{ id: 143, original: 'french', type: 'Sport Climbing', french: '8c', american: '5.14b', gradeValue: 22, color: '#9333ea' },
 
 // GradeValue 23
-{ id: 144, original: 'american', type: 'Sport Climbing', american: '5.14c', french: '9a', gradeValue: 23, color: '#7e22ce' },
-{ id: 145, original: 'french', type: 'Sport Climbing', french: '9a', american: '5.14c', gradeValue: 23, color: '#7e22ce' },
+{ id: 144, original: 'american', type: 'Sport Climbing', american: '5.14c', french: '8c+', gradeValue: 23, color: '#7e22ce' },
+{ id: 145, original: 'french', type: 'Sport Climbing', french: '8c+', american: '5.14c', gradeValue: 23, color: '#7e22ce' },
 
 // GradeValue 24
-{ id: 146, original: 'american', type: 'Sport Climbing', american: '5.14d', french: '9a+', gradeValue: 24, color: '#6b21a8' },
-{ id: 147, original: 'french', type: 'Sport Climbing', french: '9a+', american: '5.14d', gradeValue: 24, color: '#6b21a8' },
+{ id: 146, original: 'american', type: 'Sport Climbing', american: '5.14d', french: '9a', gradeValue: 24, color: '#6b21a8' },
+{ id: 147, original: 'french', type: 'Sport Climbing', french: '9a', american: '5.14d', gradeValue: 24, color: '#6b21a8' },
 
 // GradeValue 25
-{ id: 148, original: 'american', type: 'Sport Climbing', american: '5.15a', french: '9b', gradeValue: 25, color: '#581c87' },
-{ id: 149, original: 'french', type: 'Sport Climbing', french: '9b', american: '5.15a', gradeValue: 25, color: '#581c87' },
+{ id: 148, original: 'american', type: 'Sport Climbing', american: '5.15a', french: '9a+', gradeValue: 25, color: '#581c87' },
+{ id: 149, original: 'french', type: 'Sport Climbing', french: '9a+', american: '5.15a', gradeValue: 25, color: '#581c87' },
 
 // GradeValue 26
-{ id: 150, original: 'american', type: 'Sport Climbing', american: '5.15b', french: '9b+', gradeValue: 26, color: '#3b0764' },
-{ id: 151, original: 'french', type: 'Sport Climbing', french: '9b+', american: '5.15b', gradeValue: 26, color: '#3b0764' },
+{ id: 150, original: 'american', type: 'Sport Climbing', american: '5.15b', french: '9b', gradeValue: 26, color: '#3b0764' },
+{ id: 151, original: 'french', type: 'Sport Climbing', french: '9b', american: '5.15b', gradeValue: 26, color: '#3b0764' },
 
 // GradeValue 27
-{ id: 152, original: 'american', type: 'Sport Climbing', american: '5.15c', french: '9c', gradeValue: 27, color: '#1e0442' },
-{ id: 153, original: 'french', type: 'Sport Climbing', french: '9c', american: '5.15c', gradeValue: 27, color: '#1e0442' },
+{ id: 152, original: 'american', type: 'Sport Climbing', american: '5.15c', french: '9b+', gradeValue: 27, color: '#1e0442' },
+{ id: 153, original: 'french', type: 'Sport Climbing', french: '9b+', american: '5.15c', gradeValue: 27, color: '#1e0442' },
 
 // GradeValue 28
-{ id: 154, original: 'american', type: 'Sport Climbing', american: '5.15d', french: '9c+', gradeValue: 28, color: '#0f0221' },
-{ id: 155, original: 'french', type: 'Sport Climbing', french: '9c+', american: '5.15d', gradeValue: 28, color: '#0f0221' },
+{ id: 154, original: 'american', type: 'Sport Climbing', american: '5.15d', french: '9c', gradeValue: 28, color: '#0f0221' },
+{ id: 155, original: 'french', type: 'Sport Climbing', french: '9c', american: '5.15d', gradeValue: 28, color: '#0f0221' },
 
 ];
